c27daa822efb067c156a6209b9387bfde3b953c0
ferrylinxx <ferrangarola22@gmail.com>
2025-12-02 10:34:05 +0100

    fix: dynamic import for lenis to avoid SSR issues

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,41 +1,39 @@
 'use client'
 
 import { useEffect, useRef } from 'react'
-import Lenis from 'lenis'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger)
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
-  const lenisRef = useRef<Lenis | null>(null)
+  const lenisRef = useRef<any>(null)
 
   useEffect(() => {
-    // Inicializar Lenis
-    lenisRef.current = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      orientation: 'vertical',
-      gestureOrientation: 'vertical',
-      smoothWheel: true,
-      touchMultiplier: 2,
-    })
+    // Importación dinámica de Lenis para evitar problemas de SSR
+    import('lenis').then((LenisModule) => {
+      const Lenis = LenisModule.default
 
-    // Conectar Lenis con GSAP ScrollTrigger
-    lenisRef.current.on('scroll', ScrollTrigger.update)
+      lenisRef.current = new Lenis({
+        duration: 1.2,
+        easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        orientation: 'vertical',
+        gestureOrientation: 'vertical',
+        smoothWheel: true,
+        touchMultiplier: 2,
+      })
 
-    gsap.ticker.add((time) => {
-      lenisRef.current?.raf(time * 1000)
-    })
+      lenisRef.current.on('scroll', ScrollTrigger.update)
 
-    gsap.ticker.lagSmoothing(0)
-
-    // Limpiar al desmontar
-    return () => {
-      lenisRef.current?.destroy()
-      gsap.ticker.remove((time) => {
+      gsap.ticker.add((time) => {
         lenisRef.current?.raf(time * 1000)
       })
+
+      gsap.ticker.lagSmoothing(0)
+    })
+
+    return () => {
+      lenisRef.current?.destroy()
     }
   }, [])
 
